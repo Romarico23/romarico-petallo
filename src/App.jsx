31c15005680dcc24e19beb671602a8594c3bd389
Romarico23 <108353329+Romarico23@users.noreply.github.com>
2023-04-21 20:39:51 +0800

    adding dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 
 function App() {
