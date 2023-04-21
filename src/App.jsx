85e39a90ed5190c335143703899935f98e6b4c54
Romarico23 <108353329+Romarico23@users.noreply.github.com>
2023-04-21 18:10:20 +0800

    adding dist

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   // });
   return (
     <div className="App">
-      <Router>
+      <Router basename="/romarico-petallo">
         <Navbar />
         <AnimatedRoutes />
       </Router>
