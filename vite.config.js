6d28eb25be3545f88e4f65407e3ad5bbf0f3431e
Romarico23 <108353329+Romarico23@users.noreply.github.com>
2023-04-21 18:01:54 +0800

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://romarico23.github.io/romarico-petallo",
+  base: "/romarico-petallo",
   plugins: [react()],
 });
