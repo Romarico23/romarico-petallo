ada286e3f40fb7f72ebf3e0df9aa89e2553a7507
Romarico23 <108353329+Romarico23@users.noreply.github.com>
2024-10-05 10:56:49 +0800

    updated projects and about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,23 @@ import "./about.css"
 import { motion as m } from "framer-motion"
 
 export const About = () => {
+	const listVariants = {
+		hidden: {
+			opacity: 0,
+		},
+		visible: {
+			opacity: 1,
+			transition: {
+				staggerChildren: 0.2,
+			},
+		},
+	}
+
+	const itemVariants = {
+		hidden: { opacity: 0, y: 20 },
+		visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+	}
+
 	return (
 		<m.section
 			className="about"
@@ -17,34 +34,51 @@ export const About = () => {
 				</h1>
 				<h3>Web Developer!</h3>
 				<p>
-					Hi! My name is Romarico and I enjoy creating things that live on the
-					internet. My interest in web development started back in 2022 when I
-					decided to try learning about building websites using Notepad it
-					taught me a lot about HTML & CSS.
+					Hi! My name is Romarico, and I have a passion for creating things that
+					live on the internet. My journey into web development began in 2022
+					when I started learning how to build websites using Notepad. That
+					experience gave me a solid foundation in HTML and CSS, sparking my
+					interest in the field.
 				</p>
 				<p>
-					Currently, I am working as an online marketer using Facebook
-					marketplace and I'm developing a mobile app and website for my
-					projects.
+					Currently, I work as a Flutter Mobile Developer at SupSoft Tech, where
+					I develop mobile applications. In addition, I’m working on several
+					personal projects involving both mobile apps and websites.
 				</p>
 				<p>
-					Here are the technologies/languages I've been working with recently:
+					Here are the technologies and languages I’ve been working with
+					recently:
 				</p>
-				<ul>
-					<li>Flutter</li>
-					<li>React JS</li>
-					<li>Firebase</li>
-					<li>Laravel</li>
-					<li>Node JS</li>
-					<li>Dart</li>
-					<li>Javascript</li>
-					<li>PHP</li>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>SASS</li>
-					<li>REST API</li>
-					<li>MYSQL</li>
-				</ul>
+				<m.ul
+					variants={listVariants}
+					initial="hidden"
+					animate="visible"
+					className="tech-list"
+				>
+					{[
+						"Flutter",
+						"React JS",
+						"React Native",
+						"Firebase",
+						"Laravel",
+						"Node JS",
+						"Dart",
+						"Javascript",
+						"Typescript",
+						"PHP",
+						"HTML",
+						"Tailwind CSS",
+						"CSS",
+						"SASS",
+						"Bootstrap",
+						"REST API",
+						"MYSQL",
+					].map((tech, index) => (
+						<m.li key={index} variants={itemVariants}>
+							{tech}
+						</m.li>
+					))}
+				</m.ul>
 			</div>
 		</m.section>
 	)
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { motion as m } from "framer-motion"
-import { BiDownload, BiLinkExternal } from "react-icons/bi"
+import { BiDownload, BiLinkExternal, BiVideo } from "react-icons/bi"
+import { FaGithub } from "react-icons/fa"
 import "./projects.css"
 import { NavLink } from "react-router-dom"
 
 export const Projects = () => {
+	const [isModalOpen, setModalOpen] = useState(false)
+
+	const toggleModal = () => {
+		setModalOpen(!isModalOpen)
+	}
+
+	// Close the modal if clicking outside the modal content
+	const handleOverlayClick = (e) => {
+		if (e.target === e.currentTarget) {
+			setModalOpen(false)
+		}
+	}
+
 	return (
 		<m.section
 			className="projects"
@@ -22,12 +36,20 @@ export const Projects = () => {
 						<img src="assets/instagram.jpg" alt="Mobile App" loading="lazy" />
 						<div className="projects-layer projects-layer-mobile-app">
 							<h4>Instagram Clone App</h4>
-							<NavLink
-								to="https://drive.google.com/file/d/1UwCWhGkFxJrbBfj1sl_yNxOFDf9D9NBt/view?usp=drive_link"
-								target="_blank"
-							>
-								<BiDownload />
-							</NavLink>
+							<div>
+								<NavLink
+									to="https://drive.google.com/file/d/1UwCWhGkFxJrbBfj1sl_yNxOFDf9D9NBt/view?usp=drive_link"
+									target="_blank"
+								>
+									<BiDownload />
+								</NavLink>
+								<NavLink
+									to="https://github.com/Romarico23/instagram_clone"
+									target="_blank"
+								>
+									<FaGithub />
+								</NavLink>
+							</div>
 						</div>
 					</div>
 					<div className="projects-box projects-box-mobile-app">
@@ -37,52 +59,96 @@ export const Projects = () => {
 								Tiktok <br />
 								Clone App
 							</h4>
-							<NavLink
-								to="https://drive.google.com/file/d/14mgGuGb2s2DyUfIsDltl7ZB_OeI9DN6M/view?usp=drive_link"
-								target="_blank"
-							>
-								<BiDownload />
-							</NavLink>
+							<div>
+								<NavLink
+									to="https://drive.google.com/file/d/14mgGuGb2s2DyUfIsDltl7ZB_OeI9DN6M/view?usp=drive_link"
+									target="_blank"
+								>
+									<BiDownload />
+								</NavLink>
+								<NavLink
+									to="https://github.com/Romarico23/tiktok_clone"
+									target="_blank"
+								>
+									<FaGithub />
+								</NavLink>
+							</div>
 						</div>
 					</div>
 					<div className="projects-box">
 						<img src="assets/gjservices.png" alt="Project-1" />
 						<div className="projects-layer">
 							<h4>GJ Services</h4>
-							<NavLink
-								to="https://romarico23.github.io/gjservices/"
-								target="_blank"
-							>
-								<BiLinkExternal />
-							</NavLink>
+							<div>
+								<NavLink
+									to="https://romarico23.github.io/gjservices/"
+									target="_blank"
+								>
+									<BiLinkExternal />
+								</NavLink>
+								<NavLink
+									to="https://github.com/Romarico23/gjservices"
+									target="_blank"
+								>
+									<FaGithub />
+								</NavLink>
+							</div>
 						</div>
 					</div>
 					<div className="projects-box">
-						<img src="assets/eshopee.png" alt="Project-2" />
-						<div className="projects-layer">
-							<h4>Shopping Cart</h4>
-							<NavLink
-								to="https://romarico23.github.io/eshopee/"
-								target="_blank"
-							>
-								<BiLinkExternal />
-							</NavLink>
+						<img src="assets/rgsweb.png" alt="Project-2" />
+						<div className="projects-layer projects-layer-rgsweb">
+							<h4>Music Store w/ Admin Dashboard</h4>
+							<div>
+								<button onClick={toggleModal}>
+									<BiVideo />
+								</button>
+								<NavLink
+									to="https://github.com/Romarico23/rgsaudimusic"
+									target="_blank"
+								>
+									<FaGithub />
+								</NavLink>
+							</div>
 						</div>
 					</div>
 					<div className="projects-box">
 						<img src="assets/chat.png" alt="Project-3" />
 						<div className="projects-layer">
 							<h4>Chat Application</h4>
-							<NavLink
-								to="https://romarico23.github.io/chat-application/#/login"
-								target="_blank"
-							>
-								<BiLinkExternal />
-							</NavLink>
+							<div>
+								<NavLink
+									to="https://romarico23.github.io/chat-application/#/login"
+									target="_blank"
+								>
+									<BiLinkExternal />
+								</NavLink>
+								<NavLink
+									to="https://github.com/Romarico23/chat-application"
+									target="_blank"
+								>
+									<FaGithub />
+								</NavLink>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+			{/* Modal */}
+			{isModalOpen && (
+				<div className="modal-overlay" onClick={handleOverlayClick}>
+					<div className="modal" onClick={(e) => e.stopPropagation()}>
+						<h2>Demo Video</h2>
+						<video controls>
+							<source src="assets/rgswebdemo.mp4" type="video/mp4" />
+							Your browser does not support the video tag.
+						</video>
+						<button className="close-btn" onClick={toggleModal}>
+							Close
+						</button>
+					</div>
+				</div>
+			)}
 		</m.section>
 	)
 }
